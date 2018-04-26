46e34e50008f405f28210f606f082c41d54c5a2d
Mathieu Tarral <mathieu.tarral@protonmail.com>
2018-04-26 10:42:12 +0300

    push to r2vmi repo

---
+++ io_vmi.h
@@ -0,0 +1,25 @@
+#ifndef IO_VMI_H
+#define IO_VMI_H
+
+#include <libvmi/libvmi.h>
+#include <libvmi/events.h>
+#include <libvmi/libvmi_extra.h>
+#include <libvmi/x86.h>
+#include <glib.h>
+
+typedef struct {
+    char *vm_name;
+    bool url_identify_by_name;
+    char* proc_name;
+    int pid;
+    uint64_t pid_cr3;
+    int current_vcpu;
+    vmi_instance_t vmi;
+    bool attached;
+    vmi_event_t *sstep_event;
+    // table [rip] -> [vmi_event_t]
+    // event might be int3 or mem_event
+    GHashTable *bp_events_table;
+} RIOVmi;
+
+#endif // IO_VMI_H
