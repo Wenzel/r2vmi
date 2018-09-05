dbe997dbcbdc18ae3b6a40b1aa666ca0a87048dc
Mathieu Tarral <mathieu.tarral@protonmail.com>
2018-09-05 12:02:34 +0300

    Makefile changes to be compatible with r2 2.9.0

--- r2.mk
+++ r2.mk
@@ -1,4 +1,4 @@
 SO_EXT=so
 R2_CFLAGS+=$(shell pkg-config --cflags r_bin r_util)
 R2_LDFLAGS+=$(shell pkg-config --libs r_util r_bin)
-R2_USER_PLUGINS=$(shell r2 -HUSER_PLUGINS)
+R2_USER_PLUGINS=$(shell r2 -HR2_USER_PLUGINS)
