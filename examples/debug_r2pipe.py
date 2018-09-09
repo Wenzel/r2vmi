ff45b9d6d6ba63c05c13b9e51381f7bcb9232681
Mathieu Tarral <mathieu.tarral@protonmail.com>
2018-09-10 01:06:26 +0300

    add debug_r2pipe script

---
+++ examples/debug_r2pipe.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Watch Windows syscalls and print their ObjectName parameter
+
+"""
+watch_syscall.
+
+Usage:
+    watch_syscall.py [options] <vm_name> <target>
+
+Options:
+    -h --help                               Show this screen.
+    --version                               Show version.
+"""
+
+
+# stdlib
+import os
+import sys
+import logging
+import struct
+import signal
+from pprint import pprint
+from pathlib import Path
+
+# 3rd
+import r2pipe
+from docopt import docopt
+from IPython import embed
+
+def main(args):
+    level = logging.INFO
+    logging.basicConfig(level=level)
+
+    vm_name = args['<vm_name>']
+    target = args['<target>']
+
+    # init r2vmi
+    r2_url = "vmi://{}:{}".format(vm_name,target)
+    r2 = r2pipe.open(r2_url, ["-d"])
+    output = r2.cmd('pd 10')
+    logging.info("size of output: %s", len(output))
+    logging.info(output)
+
+
+
+if __name__ == '__main__':
+   args = docopt(__doc__, version='0.1')
+   ret = main(args)
+   sys.exit(ret)
