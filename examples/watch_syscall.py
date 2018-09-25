3186764c88b6fe441cad81ebe8946a759f2c853d
Mathieu Tarral <mathieu.tarral@protonmail.com>
2018-09-25 04:25:37 +0300

    remove manual singlestep and fix Rekall syscall address

--- examples/watch_syscall.py
+++ examples/watch_syscall.py
@@ -50,7 +50,7 @@ def read_field(r2, win_field, from_addr):
 
 def main(args):
     level = logging.INFO
-    logging.basicConfig(stream=sys.stdout, level=level)
+    logging.basicConfig(level=level)
     # catch SIGINT
     signal.signal(signal.SIGINT, sigint_handler)
 
@@ -62,12 +62,19 @@ def main(args):
     rekall = RekallVMI(vm_name, 'xen')
     # get syscall
     *rest, syscall_addr = rekall.find_syscall(syscall_name)
+    # rekall removed the high part of the address (ignored in IA32e paging)
+    # this will make virtual address comparison fail in radare2
+    # small fix to restore that part
+    syscall_addr |= 0xffff << 48
     # get some _OBJECT_ATTRIBUTES fields/subfields offsets & size
     win_types = rekall.get_winobj_fields()
 
     # init r2vmi
     r2_url = "vmi://{}:{}".format(vm_name,target)
     r2 = r2pipe.open(r2_url, ["-d", "-2"])
+
+    # make sure we use software breakpoints (faster)
+    r2.cmd('e dbg.hwbp=false')
 
     logging.info('Setting breakpoint on %s @%s', syscall_name, hex(syscall_addr))
     r2.cmd('db {}'.format(hex(syscall_addr)))
@@ -77,6 +84,7 @@ def main(args):
     while not interrupted:
         # continue
         r2.cmd('dc')
+        # get registers
         registers = r2.cmdj('drj')
         # bp hit !
         object_attributes_addr = registers['r8']
@@ -85,8 +93,6 @@ def main(args):
         # read UNICODE_STRING buffer
         output = r2.cmd('psw @{}'.format(hex(buffer_addr)))
         logging.info('%s - @%s: %s', target, syscall_name, output)
-        # single step to avoid hitting same breakpoint
-        r2.cmd('ds')
 
 
 if __name__ == '__main__':
